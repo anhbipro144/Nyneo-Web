6cc1a90662a04a7a3a6f3ac3b971bb5e6dd51ab9
nbi67 <nbi67@NYNEO>
2023-02-26 17:06:25 +0700

    Add Menu Unstack Effect

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -11,6 +11,12 @@ $(document).ready(function () {
     $("#navMenu").toggleClass("close-menu");
     menuIcon.toggleClass("bar-state-menu");
     menuIcon.toggleClass("x-state-menu");
+
+    $("#navMenu ul li").toggleClass("menu-item-origin");
+    $("#navMenu ul li:nth-child(1)").toggleClass("menu-item-1");
+    $("#navMenu ul li:nth-child(2)").toggleClass("menu-item-2");
+    $("#navMenu ul li:nth-child(3)").toggleClass("menu-item-3");
+    $("#navMenu ul li:nth-child(4)").toggleClass("menu-item-4");
   });
 
   // $("#close-menu-btn").click(function () {
